ae5fac47029a28b2a494360de06abe62d693adc3
t993546359 <993546359@qq.com>
2018-05-15 14:02:20 +0800

    lab

--- PC_ping.c
+++ PC_ping.c
@@ -13,9 +13,15 @@
 #include <sys/time.h>
 #include <assert.h>
 
+
+const char *IFACE_NAME = "eth0";
 char *LOCAL_IP = "192.168.2.2";
 char *DST_IP = "192.168.3.2";
 #define BUFFER_MAX 1024
+#define ICMP_HDR_LEN sizeof(struct icmphdr)
+#define ICMP_PACKET_LEN (ICMP_HDR_LEN+sizeof(struct timeval))
+
+
 char send_buffer[BUFFER_MAX];
 char recv_buffer[BUFFER_MAX];
 char PC_HWADDR[ETH_ALEN] = {0x00,0x0c,0x29,0x0a,0xe1,0xf9}; // U571
@@ -49,6 +55,21 @@ int main()
     int sockfd;
 
 	sockfd = Socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    int uid = getpid();
+
+    struct ifreq ifreq_i;
+	memset(&ifreq_i,0,sizeof(ifreq_i));
+	//giving name of Interface
+	strncpy(ifreq_i.ifr_name, IFACE_NAME, IFNAMSIZ-1); 
+	//getting Interface Index 
+	Ioctl(sockfd, SIOCGIFINDEX, &ifreq_i); 
+	sadr_ll.sll_ifindex = ifreq_i.ifr_ifindex; // index of interface
+
+	sadr_ll.sll_halen = ETH_ALEN;
+	sadr_ll.sll_family = AF_PACKET;
+	memcpy(sadr_ll.sll_addr, NEXT_HWADDR, ETH_ALEN);
+
+    int sequence = 1;
 
     // send a packet
     memset(send_buffer,0,BUFFER_MAX);
@@ -78,7 +99,8 @@ int main()
     icmp_head->type  = ICMP_ECHO; //8 – Echo Request
 	icmp_head->code  = 0;
 	// icmp_hdr->checksum = 0;
-	icmp_head->un.echo.id = htons(getpid());
+	//icmp_head->un.echo.id = htons(getpid());
+    icmp_head->icmp_id = uid;
 	icmp_head->un.echo.sequence = htons(sequence);
 
     gettimeofday((struct timeval *)((char *)icmp_head+sizeof(struct icmphdr)),NULL);
@@ -100,16 +122,34 @@ int main()
     // reply part
     while(1)
    {
-       struct ethhdr *re_eth_head = (struct ethhdr *)(recv_buffer);
-        struct iphdr *ip_head = (struct iphdr *)(send_buffer + sizeof(ethhdr));
-        if(strncmp(re_eth_head->h_dest,LOCAL_IP,ETH_ALEN) != 0 
+        struct ethhdr *re_eth_head = (struct ethhdr *)(recv_buffer);
+        struct iphdr *re_ip_head = (struct iphdr *)(recv_buffer + sizeof(ethhdr));
+        struct icmphdr *re_icmp_head = (struct icmphdr *)(recv_buffer + sizeof(ethhdr) + sizeof(iphdr));
+        struct timeval *send_tv  = (struct timeval *)(icmp_head + 1);
+        
+        if(strncmp(re_eth_head->h_dest,PC_HWADDR,ETH_ALEN) != 0 
         || re_eth_head->h_proto != htons(ETH_P_IP))
         {
             continue;
         }
+        if(icmp_head->type != ICMP_ECHOREPLY || icmp->icmp_id != pid )
+            continue;
+        struct in_addr temp;
+        inet_aton(LOCAL_IP,&temp);
+        if(re_ip_head->d_addr != temp.s_addr)
+            continue;
         
+        struct timeval now_tv;
+	    gettimeofday(&now_tv,NULL);
+
         
-  
+        printf("%d bytes from %s:\ticmp_seq=%d\tttl=%d\ttime=%.1fms\n",
+		ICMP_PACKET_LEN,	
+		DST_IP,
+		ntohs(icmp_hdr->un.echo.sequence ),
+		ip_hdr->ttl,
+		1000*(now_tv.tv_sec-send_tv->tv_sec)+(now_tv.tv_usec-send_tv->tv_usec)/1000.0
+		);
 
    }
 }
